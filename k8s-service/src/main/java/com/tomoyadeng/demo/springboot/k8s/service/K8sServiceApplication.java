b1fba286d3c91b32ae49e52c5af25d65da12523f
tomoyadeng <tomoyadeng@gmail.com>
2018-09-11 23:36:47 +0800

    add module k8s-service

---
+++ k8s-service/src/main/java/com/tomoyadeng/demo/springboot/k8s/service/K8sServiceApplication.java
@@ -0,0 +1,12 @@
+package com.tomoyadeng.demo.springboot.k8s.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8sServiceApplication.class, args);
+    }
+}
